a27ccfa9aa52abed64a1774e1647487f831ab9e7
Goutte <antoine.goutenoir@gmail.com>
2016-06-01 06:37:53 +0200

    Throttling could be done by https://github.com/jaytaph/RateLimitBundle with memcache.

--- cron/cron.php
+++ cron/cron.php
@@ -1,16 +1,15 @@
 <?php
 
+#
 # The purpose of this file is to be called in the CLI by CRON, like so:
 # php cron/cron.php daily
-
-
-// fixme: ensure this cannot be called more than once per minute (or hour) DDOS!
-// Because I want to be able to call this CRON task whenever I want,
-// but it's an expensive task, so it puts a heavy load on the server.
-// This is a very good entry point for DDOSes.
-
-// todo: there's probably a simpler way we can do this only with web/app.php
-//       this file should be removed if we can.
+#
+# It should always be safe to call multiple times.
+#
+# NOTE
+# there's probably a simpler way we can do this only with web/app.php
+# this file should be removed if we can.
+#
 
 // For every line in this config there must exist respective routes and
 // controller actions.
@@ -18,7 +17,6 @@ $freqs = array(
     'monkey',
     'daily',
 );
-
 
 $freq = $argv[1];
 if ( ! in_array($freq, $freqs)) {
